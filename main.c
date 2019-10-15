969f4bf24eec936f0ce6ce9810db231d087fe543
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-10-15 20:27:08 +0900

    06_5

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void)
+ int main(void)
+ {
+ 	int sum;
+ 	sum = compute_sum(100);
+	printf("sum = %d\n", sum);
+ }
+int compute_sum(int n)
 {
 	int i;
-	int max=45;
-	
-	srand((unsigned)time(NULL));
-	for(i=0;i<6;i++)
-		printf("%d ",1+rand()%max);
-		
-	return 0;
-}
-
-
+	int result=0;
+	for(i=1;i<=n;i++)
+		result +=i;
+	return result;
+ } 
