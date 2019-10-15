989ef47ff782a0ab032d4fba78ae85c6f365f3c5
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-10-15 19:00:08 +0900

    06_1

--- main.c
+++ main.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int factorial(int n)
-{
-	int res=1;
-	int i;
-	for(i=1;i<=n;i++)
-	{
-		res = res * i;
-	}
-	
-	return res;
-}
-int calcCombination(int n, int r)
-{
-	int high, low;
-	high = factorial(n);
-	low = factorial(n-r)*factorial(r);
-	return (high/low);
-}
-int main(int argc, char *argv[]){
-	
-	int n, r;
-	int res;
-	
-	printf("input n and r: ");
-	scanf("%d %d",&n, &r);
-	
-	res = calcCombination(n,r);
-	printf("combination result is %d\n", res);
-	
+
+int main(void)
+ {
+ 	int i;
+ 	
+ 	for(i=0;i<10;i++)
+ 	printf("*");
+ 	for(i=0;i<10;i++)
+ 	printf("*");
+ 	for(i=0;i<10;i++)
+ 	printf("*");
+ 	
 	return 0;
 }
-
